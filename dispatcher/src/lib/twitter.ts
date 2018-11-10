233565c85173fe87ca46e992bb256069f009aa65
Ifeoluwa Arowosegbe <iifeoluwa.ao@gmail.com>
2018-11-10 11:57:38 +0100

    Refactor function to perfor all kind of message sending

--- dispatcher/src/lib/twitter.ts
+++ dispatcher/src/lib/twitter.ts
@@ -1,11 +1,8 @@
 import { hit, RequestOptions } from './request'
 import { Response, GotError } from 'got';
-import { Messages } from '../config/twitter'
 
-export function sendInvalidCommandMessage(userId: string) {
-    const messageEvent = buildMessageBody(Messages.INVALID_COMMAND, userId);
+export function sendMessage(messageEvent: any) {
     const path = 'direct_messages/events/new';
-
     return hit(path, {method: 'POST', body: messageEvent})
         .then((result: Response<object>) => {
             return result.body;
@@ -16,7 +13,7 @@ export function sendInvalidCommandMessage(userId: string) {
         });
 }
 
-function buildMessageBody(message: string, recipient: string) {
+export function buildMessageBody(message: string, recipient: string) {
     return {
         event: {
             type: 'message_create',
