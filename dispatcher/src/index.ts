5260db562b785bbfe01023a7ee51b204c941aed8
Ifeoluwa Arowosegbe <iifeoluwa.ao@gmail.com>
2018-11-12 16:07:40 +0100

    Reuse redis connections while creating queues

--- dispatcher/src/index.ts
+++ dispatcher/src/index.ts
@@ -1,8 +1,36 @@
 import Queue from "bull";
+import Redis from "ioredis"
 import { RedisConfig, QueueNames } from "./config";
 const INVALID_COMMANDS_PROCESSOR = `${__dirname}/consumers/invalid_comands.js`;
 
-const invalidCommandsConsumer = new Queue(QueueNames.invalidCommands, RedisConfig.redis.url);
+/**
+ * Create a new queue with `name`.
+ *
+ * @param {String} name
+ * @return {Queue}
+ */
+function createQueue(name: string) {
+    const client = new Redis(RedisConfig.redis.url);
+    const subscriber = new Redis(RedisConfig.redis.url);
+
+    const opts: Queue.QueueOptions = {
+        createClient: function(type) {
+            switch(type){
+            case 'client':
+                return client;
+            case 'subscriber':
+                return subscriber;
+            default:
+                return new Redis();
+            }
+        }
+    };
+
+    return new Queue(name, opts);
+
+}
+
+const invalidCommandsConsumer = createQueue(QueueNames.invalidCommands);
 
 console.log('Starting Consumers');
 invalidCommandsConsumer.process(INVALID_COMMANDS_PROCESSOR);
